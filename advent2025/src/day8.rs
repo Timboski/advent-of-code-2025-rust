09cf23ec0b3c3b5cec8227e99d1cc137ff67326a
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-19 17:50:19 +0000

    Make groups. Bug - can reuse boxes

--- advent2025/src/day8.rs
+++ advent2025/src/day8.rs
@@ -3,8 +3,8 @@ use crate::utils::read_file_lines;
 
 #[allow(dead_code)]
 pub fn main() {
-    // let path = "/workspaces/advent-of-code-2025-rust/day8-example.txt";
-    let path = "/workspaces/advent-of-code-2025-rust/day8-input.txt";
+    let path = "/workspaces/advent-of-code-2025-rust/day8-example.txt";
+    // let path = "/workspaces/advent-of-code-2025-rust/day8-input.txt";
 
     let boxes: Vec<Point3D> = read_file_lines(path)
         .unwrap()
@@ -30,29 +30,76 @@ pub fn main() {
         assert!(jb.end == other.start);
         if jb.start == other.end {
             // Duplicate entry
-            print!("Duplicate found {:?} {:?}", jb, other);
+            //print!("Duplicate found {:?} {:?}", jb, other);
             if duplicates.contains(&(jb.start)) {
-                print!(" - already in list");
+                //print!(" - already in list");
             }
             else {                
                 duplicates.push(jb.end);
             }
-            println!();
+            //println!();
         }
     }
-    println!("Duplicates found: {:?}", duplicates);
+    // println!("Duplicates found: {:?}", duplicates);
 
     // Remove duplicates
     // We must do this starting with the highest index so as not to change the duplicate indices.
     duplicates.sort();
     duplicates.reverse();
     for dup in duplicates {
-        println!("Removing: {}", dup);
+        // println!("Removing: {}", dup);
         jbs.remove(dup);
     }
 
     jbs.sort_by_key(|k| k.distance_squared);
-    for jb in jbs {println!("{:?} {:?} {:?}",jb, boxes[jb.start], boxes[jb.end])}
+    for jb in &jbs {println!("{:?} {:?} {:?}",jb, boxes[jb.start], boxes[jb.end])}
+
+    // Make groups
+    let mut groups: Vec<Vec<usize>> = Vec::new();
+    for jb in jbs.iter().take(10) {
+        println!();
+        for (index, group) in groups.iter().enumerate() {println!("{}: {:?}", index, group)};
+        let start_group = find_group(&groups, jb.start);
+        let end_group = find_group(&groups, jb.end);
+        print!("Connection {}-{}: {:?} {:?} : ", jb.start, jb.end, start_group, end_group);
+
+        match start_group {
+            None => match end_group {
+                None => {
+                    println!("New Group Needed");
+                    groups.push(vec![jb.start, jb.end]);
+                }
+                Some(end) => {
+                    println!("Add start to group {}", end);
+                    groups[end].push(jb.start);
+                }
+            }
+            Some(start) => match end_group {
+                None => {
+                    println!("Add end to group {}", start);
+                    panic!("Not implemented");
+                }
+                Some(end) => {
+                    println!("Combine Groups {} and {}", start, end);
+                    panic!("Not implemented");
+                }
+            }
+        }
+    }
+
+}
+
+fn find_group(groups: &Vec<Vec<usize>>, index: usize) -> Option<usize> {
+    print!("Find {} in {:?} ", index, groups);
+    let start_group = groups
+        .iter()
+        .enumerate()
+        .filter(|(_, members)| members.contains(&index))
+        .map(|(index, _)| index)
+        .next();
+
+    println!("-> {:?}", start_group);
+    start_group
 }
 
 #[derive(PartialEq, Eq, Debug)]
