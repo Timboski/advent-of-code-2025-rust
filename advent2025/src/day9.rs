21a2b6833ec3415cec9f3292ac08f5da67e1adab
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-22 11:59:40 +0000

    Add regression test for day 9 part 1

--- advent2025/src/day9.rs
+++ advent2025/src/day9.rs
@@ -1,11 +1,17 @@
 use crate::utils::read_file_lines;
-// use rstest::rstest;
+use rstest::rstest;
 
 #[allow(dead_code)]
 pub fn main() {
     // let path = "/workspaces/advent-of-code-2025-rust/day9-example.txt";
     let path = "/workspaces/advent-of-code-2025-rust/day9-input.txt";
 
+    let max_area = part_1(path);
+
+    println!("Biggest rectangle: {}", max_area);
+}
+
+fn part_1(path: &str) -> u64 {
     let lines = read_file_lines(path).unwrap();
     let points: Vec<(u32, u32)> = lines.iter()
         .map(
@@ -24,10 +30,25 @@ pub fn main() {
             max_area = max_area.max(area);
         }
     }
-
-    println!("Biggest rectangle: {}", max_area);
+    max_area
 }
 
 fn compute_side_length(point1: u32, point2: u32) -> u64 {
     point1.abs_diff(point2) as u64 + 1
+}
+
+
+#[rstest]
+#[case("/workspaces/advent-of-code-2025-rust/day9-example.txt", 50)]
+#[case("/workspaces/advent-of-code-2025-rust/day9-input.txt", 4748985168)]
+fn test_part1_answers(
+    #[case] path: &str,
+    #[case] expected_area: u64
+)
+{
+    // Act
+    let max_area = part_1(path);
+
+    // Assert
+    assert_eq!(max_area, expected_area);
 }
