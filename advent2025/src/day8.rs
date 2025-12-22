0d0784049661e62150b6a953d69a287e8592d78b
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-22 09:37:05 +0000

    Add regression test

--- advent2025/src/day8.rs
+++ advent2025/src/day8.rs
@@ -1,19 +1,21 @@
 use crate::utils::read_file_lines;
-// use rstest::rstest;
+use rstest::rstest;
 
 #[allow(dead_code)]
 pub fn main() {
     // let path = "/workspaces/advent-of-code-2025-rust/day8-example.txt";
+    // let max_number_of_connections= 10;
+
     let path = "/workspaces/advent-of-code-2025-rust/day8-input.txt";
+    let max_number_of_connections= 1000;
 
-    let max_number_of_connections= 1000000000;
+    let (size, distance) = find_connections(path, max_number_of_connections);
 
-    let answer = part1(path, max_number_of_connections);
-
-    println!("Size of top 3 groups: {}", answer);
+    println!("Size of top 3 groups: {}", size);
+    println!("Distance to socket: {}", distance);
 }
 
-fn part1(path: &str, max_number_of_connections: usize) -> usize {
+fn find_connections(path: &str, max_number_of_connections: usize) -> (usize, u128) {
     // Read the file
     let boxes: Vec<Point3D> = read_file_lines(path)
         .unwrap()
@@ -39,12 +41,22 @@ fn part1(path: &str, max_number_of_connections: usize) -> usize {
     for jb in jbs.iter().take(20) {println!("{:?} {:?} {:?}",jb, boxes[jb.start], boxes[jb.end])}
 
     // Make groups
+    let mut size_of_top_three_goups= 0;
+    let mut distance_to_socket= 0;
     let mut connections: Vec<Connection> = Vec::new();
     let mut groups: Vec<Vec<usize>> = Vec::new();
     for jb in &jbs {
         println!("Num connections made: {}", connections.len());
         for (index, group) in groups.iter().enumerate() {println!("{}: {:?}", index, group)};
-        if connections.len() >= max_number_of_connections { break }
+        if connections.len() == max_number_of_connections && size_of_top_three_goups == 0{ 
+            // We are at the limit - compute the Part 1 result
+            print!("PART1 COMPLETE");
+            groups.sort_by_key(|g| g.len());
+            groups.reverse();
+            size_of_top_three_goups = groups.iter().take(3).map(|g| g.len()).product();
+            println!(": Size = {}", size_of_top_three_goups);
+            println!();
+         }
         if groups.len() == 1 && groups[0].len() == boxes.len() {break}
         println!();
         if connections.iter().any(|c| c.is_same(&jb)) {             
@@ -55,7 +67,8 @@ fn part1(path: &str, max_number_of_connections: usize) -> usize {
         let start_group = find_group(&groups, jb.start);
         let end_group = find_group(&groups, jb.end);
         println!("Connection {}-{}: {:?} {:?} : ", jb.start, jb.end, start_group, end_group);
-        println!("X-Distance {:?}-{:?}: {}", boxes[jb.start], boxes[jb.end], boxes[jb.start].x as u128 * boxes[jb.end].x as u128);
+        distance_to_socket = boxes[jb.start].x as u128 * boxes[jb.end].x as u128;
+        println!("X-Distance {:?}-{:?}: {}", boxes[jb.start], boxes[jb.end], distance_to_socket);
         connections.push(Connection { start: jb.start, end: jb.end });
 
         match start_group {
@@ -92,11 +105,7 @@ fn part1(path: &str, max_number_of_connections: usize) -> usize {
 
     println!();
     println!("FINISHED");
-    groups.sort_by_key(|g| g.len());
-    groups.reverse();
-    for (index, group) in groups.iter().enumerate() {println!("{}: {:?}", index, group)};
-    let answer: usize = groups.iter().take(3).map(|g| g.len()).product();
-    answer
+    (size_of_top_three_goups, distance_to_socket)
 }
 
 fn find_group(groups: &Vec<Vec<usize>>, index: usize) -> Option<usize> {
@@ -163,4 +172,23 @@ impl Connection {
 
         return false;
     }
+}
+
+
+#[rstest]
+#[case("/workspaces/advent-of-code-2025-rust/day8-example.txt", 10, 40, 25272)]
+#[case("/workspaces/advent-of-code-2025-rust/day8-input.txt", 1000, 57564, 133296744)]
+fn test_answers(
+    #[case] path: &str,
+    #[case] limit: usize,
+    #[case] expected_size: usize,
+    #[case] expected_last_connection: u128
+)
+{
+    // Act
+    let (size, last_connection) = find_connections(path, limit);
+
+    // Assert
+    assert_eq!(size, expected_size);
+    assert_eq!(last_connection, expected_last_connection);
 }
