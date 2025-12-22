86e5fdd814b49bd7602b1a82d8c76e2d289fd627
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-22 15:34:11 +0000

    Add regression test (day 9 part 2)

--- advent2025/src/day9.rs
+++ advent2025/src/day9.rs
@@ -7,19 +7,19 @@ pub fn main() {
     // let path = "/workspaces/advent-of-code-2025-rust/day9-example.txt";
     let path = "/workspaces/advent-of-code-2025-rust/day9-input.txt";
 
-    let area = part_2(path);
+    let area1 = part_1(path);
+    let area2 = part_2(path);
 
-    println!("Biggest rectangle: {}", area);
+    println!("Biggest rectangle overall: {}", area1);
+    println!("Biggest rectangle just green and red: {}", area2);
 }
 
-#[allow(dead_code)]
 fn part_1(path: &str) -> u64 {
     let points = read_points_from_file(path);
     let areas = compute_rectangles(points);
     areas.first().unwrap().0
 }
 
-#[allow(dead_code)]
 fn compute_rectangles(points: Vec<(u32, u32)>) -> Vec<(u64, u32, u32, u32, u32)> {
     let mut areas = Vec::new();
     for point1 in &points {
@@ -175,6 +175,21 @@ fn test_part1_answers(
 {
     // Act
     let max_area = part_1(path);
+
+    // Assert
+    assert_eq!(max_area, expected_area);
+}
+
+#[rstest]
+#[case("/workspaces/advent-of-code-2025-rust/day9-example.txt", 24)]
+#[case("/workspaces/advent-of-code-2025-rust/day9-input.txt", 1550760868)]
+fn test_part2_answers(
+    #[case] path: &str,
+    #[case] expected_area: u64
+)
+{
+    // Act
+    let max_area = part_2(path);
 
     // Assert
     assert_eq!(max_area, expected_area);
