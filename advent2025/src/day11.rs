618ccc46b4cf94bffcf84a87e4b20b0aec7ec0c6
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-23 16:32:30 +0000

    FIlter paths for dac and fft

--- advent2025/src/day11.rs
+++ advent2025/src/day11.rs
@@ -4,14 +4,17 @@ use std::collections::HashMap;
 #[allow(dead_code)]
 pub fn main() {
     // let path = "/workspaces/advent-of-code-2025-rust/day11-example.txt";
+    // let path = "/workspaces/advent-of-code-2025-rust/day11-example2.txt";
     let path = "/workspaces/advent-of-code-2025-rust/day11-input.txt";
 
     let devices: HashMap<String, Vec<String>> = read_file_lines(path).unwrap().iter().map(|l| get_device_details(l)).collect();
+    println!("{:?}", devices);
 
     let mut paths_found = 0;
-    let mut tracers = vec!["you".to_string()];
+    let mut valid_paths_found = 0;
+    let mut tracers = vec![("svr".to_string(), false, false)];
     loop {
-        let tracer = match tracers.pop() {
+        let (tracer, dac_seen, fft_seen) = match tracers.pop() {
             Some(t) => t,
             None => { break; },
         };
@@ -19,13 +22,17 @@ pub fn main() {
         for connection in devices.get(&tracer).unwrap() {
             if connection == "out" {
                 paths_found += 1;
+                if dac_seen && fft_seen { valid_paths_found += 1 }
             } else {
-                tracers.push(connection.clone());
+                let now_dac_seen = dac_seen || connection == "dac";
+                let now_fft_seen = fft_seen || connection == "fft";
+                tracers.push((connection.clone(), now_dac_seen, now_fft_seen));
             }
         }
     }
 
     println!("Paths found: {}", paths_found);
+    println!("Valid Paths found: {}", valid_paths_found);
 }
 
 fn get_device_details(devce_definition: &str) -> (String, Vec<String>) {
