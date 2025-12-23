5169f72762148e5ccef9c6cb70d5ddc87b28de61
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-23 09:26:30 +0000

    Move file decoding out of analysis

--- advent2025/src/day10.rs
+++ advent2025/src/day10.rs
@@ -15,32 +15,36 @@ pub fn main() {
 
 fn part1(path: &str) -> u32 {
     let lines = read_file_lines(path).unwrap();
+
     let mut total_presses = 0;
     for line in lines {
-        let presses = find_fewest_button_presses(line);
+        let parts =  line.split_once("]").unwrap();
+        let desired_state = find_desired_state(parts.0);
+        println!("Desired State: {:?}", desired_state);
+        let (line_fragment_2, line_fragment_3) = parts.1.split_once("{").unwrap();
+        let masks: Vec<u16> = line_fragment_2
+            .split_whitespace()
+            .map(|s| 
+                s.trim_matches(|c| c == '(' || c == ')')
+                .split(",")
+                .map(|i| 1u16<<i.parse::<usize>().unwrap())
+                .sum()
+            )
+            .collect();
+        println!("Steps {:?}", masks);
+        println!("Joltages (unused) {:?}", line_fragment_3);
+
+
+        let presses = find_fewest_button_presses(desired_state, masks);
         println!("Fewest button presses: {}", presses);
         println!();
         total_presses += presses;
     }
+
     total_presses
 }
 
-fn find_fewest_button_presses(machine_description: String) -> u32 {
-    let parts =  machine_description.split_once("]").unwrap();
-    let desired_state = find_desired_state(parts.0);
-    println!("Desired State: {:?}", desired_state);
-    let (line_fragment_2, line_fragment_3) = parts.1.split_once("{").unwrap();
-    let masks: Vec<u16> = line_fragment_2
-        .split_whitespace()
-        .map(|s| 
-            s.trim_matches(|c| c == '(' || c == ')')
-            .split(",")
-            .map(|i| 1u16<<i.parse::<usize>().unwrap())
-            .sum()
-        )
-        .collect();
-    println!("Steps {:?}", masks);
-    println!("Joltages (unused) {:?}", line_fragment_3);
+fn find_fewest_button_presses(desired_state: u16, masks: Vec<u16>) -> u32 {
     let mut universes: BinaryHeap<(Reverse<u32>, u16, Vec<u16>)> = BinaryHeap::new();
     universes.push((Reverse(0), 0, Vec::new()));
     let mut states_seen: HashSet<u16> = HashSet::new();
@@ -60,8 +64,7 @@ fn find_fewest_button_presses(machine_description: String) -> u32 {
             let new_state = universe.1 ^ mask;
             let mut new_steps = universe.2.clone();
             new_steps.push(*mask);
-        
-        
+            
             // Don't revisit the same state twice
             if !states_seen.contains(&new_state) {
                 let new_universe = (Reverse(new_priority), new_state, new_steps);
