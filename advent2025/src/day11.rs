5cf915fd9b403a20d9b482be72a8f47431c5f6e6
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-24 12:22:30 +0000

    Redo Day 11 using Khan's algorithm

--- advent2025/src/day11.rs
+++ advent2025/src/day11.rs
@@ -7,8 +7,53 @@ pub fn main() {
     // let path = "/workspaces/advent-of-code-2025-rust/day11-example2.txt";
     let path = "/workspaces/advent-of-code-2025-rust/day11-input.txt";
 
-    let devices: HashMap<String, Vec<String>> = read_file_lines(path).unwrap().iter().map(|l| get_device_details(l)).collect();
-    println!("{:?}", devices);
+    // Build a list of the outgoing connections from each device
+    let connection: HashMap<String, Vec<String>> = read_file_lines(path).unwrap().iter().map(|l| get_device_details(l)).collect();
+    println!("Devices: {:?}", connection);
+
+    // Count the number of incoming connections into each device.
+    // NOTE: Populate from keys so that devices with no connections appear.
+    let mut indegree: HashMap<String, u32> = connection.keys().map(|k| (k.clone(), 0)).collect();
+    for links in connection.values() {
+        for link in links {
+            *indegree.entry(link.clone()).or_insert(0) += 1;
+        }
+    }
+    println!("Indegrees: {:?}", indegree);
+
+    // Create a topological ordering using Khan's algorithm
+    let mut order: Vec<String> = Vec::new();
+    loop {
+        // Get an item with 0 in links            
+        let next = match indegree.iter().find(|(_, v)| **v == 0) {
+            Some(n) => n.0.clone(),
+            None => { break }
+        };
+        indegree.remove(&next);
+        order.push(next.clone());
+        print!("Popped: {}", next);
+
+        // Remove the inlinks from this device to other devices
+        let links = &connection.get(&next);
+        match links {
+            Some(l) => {                
+                println!(" - links to {:?}", l);
+                for link in *l {
+                    assert!(!order.contains(link)); // Can't reference earler devices
+                    if indegree.contains_key(link) {                
+                        *indegree.entry(link.clone()).or_insert(0) -= 1;
+                    }
+                }
+            }
+            None => { println!(" - end point")}
+        }
+    }
+
+    println!("Remaining elements: {:?}", indegree);
+
+
+    return;
+
 
     let mut paths_found = 0;
     let mut valid_paths_found = 0;
@@ -19,7 +64,7 @@ pub fn main() {
             None => { break; },
         };
 
-        for connection in devices.get(&tracer).unwrap() {
+        for connection in connection.get(&tracer).unwrap() {
             if connection == "out" {
                 paths_found += 1;
                 if dac_seen && fft_seen { valid_paths_found += 1 }
