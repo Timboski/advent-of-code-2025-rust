081a1829715fa94c65441c15ad0648bb4ed9d715
Tim Boyle <tim.boyle@stfc.ac.uk>
2025-12-24 14:24:44 +0000

    Regression test for day 11 part 1

--- advent2025/src/day11.rs
+++ advent2025/src/day11.rs
@@ -1,3 +1,5 @@
+use rstest::rstest;
+
 use crate::utils::read_file_lines;
 use std::collections::HashMap;
 
@@ -7,6 +9,42 @@ pub fn main() {
     // let path = "/workspaces/advent-of-code-2025-rust/day11-example2.txt";
     let path = "/workspaces/advent-of-code-2025-rust/day11-input.txt";
 
+    let num_paths = part_1(path);    
+    
+    println!();
+    println!("Paths found: {}", num_paths);
+
+    return;
+}
+
+fn part_1(path: &str) -> u64 {
+    let (connection, order) = compute_connections_and_topological_order(path);
+    let scores = find_number_of_paths(connection, order);
+    scores["you"]
+}
+
+
+/// Compute the number of paths to the end for each device
+fn find_number_of_paths(connection: HashMap<String, Vec<String>>, order: Vec<String>) -> HashMap<String, u64> {
+    // Workthrough the devices in topological order assigning a score to each device.
+    // The score is the number of routes to the end from that device.
+    // This can be computed by summing the number of routes on each of its outgoing connections.
+    let mut scores = HashMap::new();
+    for device in order {
+        println!("Scoring {}", device);
+        let links = &connection.get(&device);
+        let score = match links {
+            Some(l) => { l.iter().map(|i| scores[i]).sum() }
+            None => 1u64,
+        };
+        scores.insert(device, score);
+    }
+
+    println!("Scores: {:?}", scores);
+    scores
+}
+
+fn compute_connections_and_topological_order(path: &str) -> (HashMap<String, Vec<String>>, Vec<String>) {
     // Build a list of the outgoing connections from each device
     let connection: HashMap<String, Vec<String>> = read_file_lines(path).unwrap().iter().map(|l| get_device_details(l)).collect();
     println!("Devices: {:?}", connection);
@@ -48,33 +86,34 @@ pub fn main() {
             None => { println!(" - end point")}
         }
     }
-    assert!(indegree.len() == 0); // Check all elements used up
+    assert!(indegree.len() == 0);
+    // Check all elements used up
 
     // Work backwards through the list assigning number of routes to the end for each device.
     // Number of routes is the sum of the number of routes on each of its connections.
     order.reverse();
     println!("Topological order of devices (reversed) {:?}", order);
 
-    let mut scores = HashMap::new();
-    for device in order {
-        println!("Scoring {}", device);
-        let mut score = 0;
-        let links = &connection.get(&device);
-        let score = match links {
-            Some(l) => { l.iter().map(|i| scores[i]).sum() }
-            None => 1u64,
-        };
-        scores.insert(device, score);
-    }
-
-    println!("Scores: {:?}", scores);    
-    println!();
-    println!("Paths found: {}", scores["you"]);
-
-    return;
+    (connection, order)
 }
 
 fn get_device_details(devce_definition: &str) -> (String, Vec<String>) {
     let (id, connections) = devce_definition.split_once(":").unwrap();
     ( id.to_string(), connections.split_whitespace().map(|s| s.to_string()).collect())
+}
+
+
+#[rstest]
+#[case("/workspaces/advent-of-code-2025-rust/day11-example.txt", 5)]
+#[case("/workspaces/advent-of-code-2025-rust/day11-input.txt", 500)]
+fn test_part1_answers(
+    #[case] path: &str,
+    #[case] expected_paths: u64
+)
+{
+    // Act
+    let max_area = part_1(path);
+
+    // Assert
+    assert_eq!(max_area, expected_paths);
 }
